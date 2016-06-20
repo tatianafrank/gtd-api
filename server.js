7de933343a30abdf0111686536a9f311c5ed6b85
tatiana <tatiana@onerockwell.com>
2016-06-20 11:47:08 -0400

    port

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 var routes = require('./app/routes');
 var mongo = require('mongodb');
 
-// mongo.MongoClient.connect('mongodb://localhost:27017/gtd', function(err, db){
-	// routes(app, db);
-	var server = app.listen('104.131.99.148:3000' || 3000, function () {
+mongo.MongoClient.connect('mongodb://localhost:27017/gtd', function(err, db){
+	routes(app, db);
+	var server = app.listen('104.131.99.148:80', function () {
 	    var port = server.address().port;
 	    console.log("App now running on port", port);
 	}); 
-// });
+});
