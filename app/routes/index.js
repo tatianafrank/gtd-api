be8e7810bd2bf6f5604114443afc5091c54cd23f
tatiana <tatiana@onerockwell.com>
2016-06-21 10:32:46 -0400

    consolidated conditions in buildQuery

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,51 +1,73 @@
 module.exports = function(app, db) {
 	var url = require('url');
 
-	app.get('/*', function(req, res) {
-		var parsedUrl = url.parse(req.url, true);
-		if(parsedUrl){
-			parseQuery(parsedUrl)
-		}
+	app.get('/', function(req, res) {
+		res.send('Please enter query parameters to search the database');
+	});
 
-		function parseQuery(parsedUrl) {
+	app.get('/search/*', function(req, res) {
+		var parsedUrl = url.parse(req.url, true);
+
+		if(parsedUrl){
 			var query = parsedUrl.query;
 			var queryKeys = Object.keys(query);
 
-			//Mongo find query does no take strings so must declare and nest objects for each parameter in the query below
-			var yearObj = {};
-			var countryObj = {};
-			var yrange = {};
-			var weapObj = {};
-
-			//iterate through each query parameter in url
-			for(i = 0; i < queryKeys.length; i++) {
-				var qk = queryKeys[i];
-				var qv = query[qk];
-				//for year paramater create a range value that is >= startyr and <= endyr
-				if(qk == 'startyr'){	
-					yrange['$gte'] = Number(qv);
-					yearObj['iyear'] = yrange;
-					
-				}
-				else if(qk == 'endyr') {
-					yrange['$lte'] = Number(qv);
-					yearObj['iyear'] = yrange;
-				}
-				else if(qk == 'country'){
-					var inRange = getRange(qv);
-					countryObj['country'] = inRange;
-				}
-				else if (qk = 'weapon'){
-					var inRange = getRange(qv);
-					weapObj['weaptype1'] = inRange;
-				}
+			if(queryKeys.length > 0){
+				buildQuery(query, queryKeys);
 			}
-
-			searchDb(yearObj, countryObj, weapObj);
 		}
 
-		function getRange(obj){
-		//creates "$in" object for paramaters with more than one value for a single key
+		function buildQuery(query, queryKeys) {
+
+			//Mongo find query does not take strings so must declare and nest objects for each parameter in the query below
+			var yearObj = {};
+			var yrange = {};
+			var paramArr = [];
+			
+
+			//Iterate through each query parameter provided by user in url
+			for(i = 0; i < queryKeys.length; i++) {
+				var qk = queryKeys[i]; //query key
+				var qv = query[qk]; //query value
+
+				if(qk == 'startyr'){	
+				//for year paramater create a range value that is >= start year and < end year
+					yrange['$gte'] = Number(qv);
+					yearObj['iyear'] = yrange;
+				}
+				else if(qk == 'endyr') {
+					yrange['$lt'] = Number(qv);
+					yearObj['iyear'] = yrange;
+				}
+				else if (qk == 'weaptype1' || 'attacktype1' || 'targtype1' || 'country') {
+					var inRange = getRange(qv);
+					var pObj = {};
+					pObj[qk] = inRange;
+					paramArr.push(pObj);
+				}
+				else if (qk == 'gname'){
+					if (typeof qv == 'object') {
+						var inRange = {};
+						inRange['$in'] = [];
+						for(i=0; i < qv.length; i++){
+							inRange['$in'].push(qv[i]);
+						}
+						paramArr.push(inRange);
+					} else {
+						var qObj = {};
+						qObj[qk] = qv;
+						paramArr.push(qObj);
+					}
+				}
+			}
+			paramArr.push(yearObj)
+			console.log(paramArr);
+			searchDb(paramArr);
+			//calls searchDB with all the mongo db query objects in one arrauy 
+		}
+
+		function getRange(obj) {
+			//creates "$in" object for paramaters with more than one value for a single key
 			var inRange = {};
 			inRange['$in'] = [];
 			if (typeof obj == 'object') {
@@ -58,15 +80,13 @@ module.exports = function(app, db) {
 			return inRange;
 		}
 		
-		function searchDb(yearObj, countryObj, weapObj){
-			var allQueries = [];
+		function searchDb(paramArr) {
 			var queryStr = {};
-			//push year outside of loop since it has a start and end and may be out of order
-			allQueries.push(yearObj, countryObj, weapObj);
-			queryStr['$and'] = allQueries;
-			var test = db.collection('allData').find(queryStr).toArray(function(err, data){
+			queryStr['$and'] = paramArr;
+			var result = db.collection('allData').find(queryStr).toArray(function(err, data) {
 				if(err) {
-					res.error(err);
+					console.log(err);
+					res.send(err);
 				} else {
 					res.json(data);
 				}
