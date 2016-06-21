f1875805d1f2fd41a55a077bd6025d3a7cd17052
tatiana <tatiana@onerockwell.com>
2016-06-21 11:58:24 -0400

    added method to limit rates to 10 requests per minute

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,18 @@
 module.exports = function(app, db) {
 	var url = require('url');
+	var RateLimit = require('express-rate-limit');
+ 
+	app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc) 
+	 
+	var limiter = new RateLimit({
+	  windowMs: 1*60*1000, // 15 minutes 
+	  max: 10, // limit each IP to 100 requests per windowMs 
+	  delayMs: 0, // disable delaying - full speed until the max limit is reached 
+	  message: "The maximum number of requests is 10 per minute. Please wait a few moments and try again."
+	});
+	 
+	//  apply to all requests 
+	app.use(limiter);
 
 	app.get('/', function(req, res) {
 		res.send('Please enter query parameters to search the database');
@@ -86,9 +99,9 @@ module.exports = function(app, db) {
 			var result = db.collection('allData').find(queryStr).toArray(function(err, data) {
 				if(err) {
 					console.log(err);
-					res.send(err);
+					return res.send(err);
 				} else {
-					res.json(data);
+					return res.json(data);
 				}
 				db.close();
 			});
